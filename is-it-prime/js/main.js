c3803fd9e631db8076e70b3bad8b2c55a727a0e5
Mike Spade <mikespadeny@gmail.com>
2022-05-11 07:07:02 -0600

    Create main.js

---
+++ is-it-prime/js/main.js
@@ -0,0 +1,12 @@
+function divisors(num) {
+  let divisors = []
+  for(let i=2;i < num;i++) {
+    if(num % i === 0){
+      divisors.push(i,)
+    }
+  }
+  if (divisors.length === 0){
+    return(`${num} is prime`)
+  } else {
+  return divisors
+  }
