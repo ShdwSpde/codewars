eab3a138c8cdfec3ffb6988f28b90f92b615fd91
Mike Spade <mikespadeny@gmail.com>
2022-10-13 19:11:03 -0400

    Update main.js

--- is-it-prime/js/main.js
+++ is-it-prime/js/main.js
@@ -2,7 +2,7 @@ function divisors(num) {
   let divisors = []
   for(let i=2;i < num;i++) {
     if(num % i === 0){
-      divisors.push(i,)
+      divisors.push(i)
     }
   }
   if (divisors.length === 0){
